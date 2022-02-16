398731ee1dbf274192697183b2c2367f8b4d7840
insomhsg <insomnia.chaos@gmail.com>
2022-02-17 00:04:59 +0900

    update part1

--- Test.py
+++ Test.py
@@ -6,3 +6,30 @@ b = 2
 c = 3
 d = a + b + c
 print(d)
+
+## if ~ else 
+if a > b:
+    print("a is greater then b") ## tab and enter
+else:
+    print("b is greater the a")
+
+## for 
+for a in [1, 2, 3]:
+    print(a)
+
+## whlie
+i = 0
+while i < 3:
+    i = i+1
+    print(i)
+
+## method, function
+def add(a, b):
+    return a+b
+
+re = add(12,13)
+print(re)
+
+"""
+긴 주석문
+"""
---
+++ part1.py
@@ -0,0 +1,28 @@
+"""
+수
+"""
+a = 0o177 #8진수
+b = 0x4aa #16진수
+
+a = 3
+b = 2
+c = a ** b ##제곱 
+print(c)
+
+a = 14 % 3
+print(a)
+
+"""
+문자열 자료형
+작은 따옴표, 큰 따옴표, 슬래시를 사용
+"""
+test = "\"Python is ver easy.\" he says" 
+
+# multiline
+multiline = '''
+Python
+is
+easy
+'''
+
+print(multiline)
